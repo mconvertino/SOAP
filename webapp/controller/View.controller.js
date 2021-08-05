038ecb5d3512d4facc7e2bfce2898f9feb3b32c1
Matteo Convertino <mconvertino@deloitte.it>
2021-08-05 06:53:12 +0000

    WS integration

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -10,6 +10,31 @@ sap.ui.define([
 		return Controller.extend("soap.soap.controller.View", {
 			onInit: function () {
 
-			}
+            },
+
+            resSucccess : function(data, status, req) {
+                debugger;
+            },
+
+            resError : function(data, status, req) {
+                debugger;
+            },
+
+            onSubmit : function(oEvent) {
+
+                var iNum1 = this.getView().byId("num1").getValue();
+                var iNum2 = this.getView().byId("num2").getValue();
+                //var sUrl = "https://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=AddInteger&Arg1="+ iNum1 + "&Arg2=" + iNum1;
+                var sUrl = "/soap/csp/samples/SOAP.Demo.cls?soap_method=AddInteger&Arg1="+ iNum1 + "&Arg2=" + iNum1;
+
+                $.ajax({
+                    type: "GET",
+                    url: sUrl,
+                    content: "text/xml",
+                    dataType: "xml",
+                    success: this.resSucccess,
+                    error: this.resError
+                });
+            }
 		});
 	});
